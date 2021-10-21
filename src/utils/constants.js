7496d0e21cf66c9a7d400fc108a8460b07474107
Adnan Saleem <adnan13893@gmail.com>
2021-10-21 11:40:03 +0500

    added two more projects

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,63 +1,75 @@
 export const projects = [
   {
-    name: "Birthday Reminder",
-    url: "https://birthday-reminder-list-app.netlify.app/",
+    name: 'Birthday Reminder',
+    url: 'https://birthday-reminder-list-app.netlify.app/',
     thumbnail:
-      "https://i2.paste.pics/0eef4e55f36eaaeee220d3f1a55bde99.png?trs=ef76c4cd048a989215e5f43cbfd8c1fb3ab7312e3c34710d3fda2ae9c2db3856",
-    image: "birthday-reminder.jpeg"
+      'https://i2.paste.pics/0eef4e55f36eaaeee220d3f1a55bde99.png?trs=ef76c4cd048a989215e5f43cbfd8c1fb3ab7312e3c34710d3fda2ae9c2db3856',
+    image: 'birthday-reminder.jpeg'
   },
   {
-    name: "Tours",
-    url: "https://reactjs-tours-app.netlify.app/",
-    thumbnail: "https://i2.paste.pics/23f28111201fee832112241f51d10859.png",
-    image: "tours.jpeg"
+    name: 'Tours',
+    url: 'https://reactjs-tours-app.netlify.app/',
+    thumbnail: 'https://i2.paste.pics/23f28111201fee832112241f51d10859.png',
+    image: 'tours.jpeg'
   },
   {
-    name: "Reviews",
-    url: "https://reactjs-reviews-app.netlify.app/",
-    thumbnail: "https://i2.paste.pics/adc36d7549aa616430431ee392b5d87b.png",
-    image: "reviews.jpeg"
+    name: 'Reviews',
+    url: 'https://reactjs-reviews-app.netlify.app/',
+    thumbnail: 'https://i2.paste.pics/adc36d7549aa616430431ee392b5d87b.png',
+    image: 'reviews.jpeg'
   },
   {
-    name: "Questions",
-    url: "https://react-questions-answers.netlify.app/",
-    thumbnail: "https://i2.paste.pics/104e3587e17c398a959703f62047b735.png",
-    image: "questions.jpeg"
+    name: 'Questions',
+    url: 'https://react-questions-answers.netlify.app/',
+    thumbnail: 'https://i2.paste.pics/104e3587e17c398a959703f62047b735.png',
+    image: 'questions.jpeg'
   },
   {
-    name: "Menu",
-    url: "https://reactjs-menu-app.netlify.app/",
-    thumbnail: "https://i2.paste.pics/96d624bd8e3a3e5cf51690bf3417b6a9.png",
-    image: "menu.jpeg"
+    name: 'Menu',
+    url: 'https://reactjs-menu-app.netlify.app/',
+    thumbnail: 'https://i2.paste.pics/96d624bd8e3a3e5cf51690bf3417b6a9.png',
+    image: 'menu.jpeg'
   },
   {
-    name: "Experience",
-    url: "https://experience-app-reactjs.netlify.app/",
-    thumbnail: "https://i2.paste.pics/e49f07ee62481f9b100e73da69ecc5db.png",
-    image: "experience.jpeg"
+    name: 'Experience',
+    url: 'https://experience-app-reactjs.netlify.app/',
+    thumbnail: 'https://i2.paste.pics/e49f07ee62481f9b100e73da69ecc5db.png',
+    image: 'experience.jpeg'
   },
   {
-    name: "Slider",
-    url: "https://reviews-slider-reactjs.netlify.app/",
-    thumbnail: "https://i2.paste.pics/c6c50ac109406b4a4645dc602e22249f.png",
-    image: "slider.jpeg"
+    name: 'Slider',
+    url: 'https://reviews-slider-reactjs.netlify.app/',
+    thumbnail: 'https://i2.paste.pics/c6c50ac109406b4a4645dc602e22249f.png',
+    image: 'slider.jpeg'
   },
   {
-    name: "Lorem Ipsum Generator",
-    url: "https://random-lorem-ipsum-paragraph-generator.netlify.app/",
-    thumbnail: "https://i2.paste.pics/c916d1e056de437f86fb3254305de2b2.png",
-    image: "lorem-ipsum-generator.jpeg"
+    name: 'Lorem Ipsum Generator',
+    url: 'https://random-lorem-ipsum-paragraph-generator.netlify.app/',
+    thumbnail: 'https://i2.paste.pics/c916d1e056de437f86fb3254305de2b2.png',
+    image: 'lorem-ipsum-generator.jpeg'
   },
   {
-    name: "Color Generator",
-    url: "https://reactjs-colorgenerator.netlify.app/",
-    thumbnail: "https://i2.paste.pics/60eee642d6049865326a756039396807.png",
-    image: "color-generator.jpg"
+    name: 'Color Generator',
+    url: 'https://reactjs-colorgenerator.netlify.app/',
+    thumbnail: 'https://i2.paste.pics/60eee642d6049865326a756039396807.png',
+    image: 'color-generator.jpg'
   },
   {
-    name: "To-do List",
-    url: "https://to-do-list-app-react-js.netlify.app/",
-    thumbnail: "https://i2.paste.pics/d4a709ef37ff0add847c897b8859707a.jpg",
-    image: "todo-list.jpeg"
+    name: 'To-do List',
+    url: 'https://to-do-list-app-react-js.netlify.app/',
+    thumbnail: 'https://i2.paste.pics/d4a709ef37ff0add847c897b8859707a.jpg',
+    image: 'todo-list.jpeg'
+  },
+  {
+    name: 'Cart',
+    url: 'https://cart-app-reactjs.netlify.app/',
+    thumbnail: 'https://i2.paste.pics/f0ba122b14994ad64139669db3bf9941.png',
+    image: 'cart.jpeg'
+  },
+  {
+    name: 'Sidebar and Modal',
+    url: 'https://sidebar-modal-reactjs.netlify.app/',
+    thumbnail: 'https://i2.paste.pics/17327b0965dd46d0c7e637a7ff0f84ae.png',
+    image: 'sidebar-modal.jpg'
   }
-];
+]
